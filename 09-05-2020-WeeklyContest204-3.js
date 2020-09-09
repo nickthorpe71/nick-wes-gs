8006b8b26fffdd1c2b24e91676ee3a071e1b2584
Wesley Rou <wesleyrou@gmail.com>
2020-09-08 19:03:01 -0700

    updated weekly contest 204

--- 09-05-2020-WeeklyContest204-3.js
+++ 09-05-2020-WeeklyContest204-3.js
@@ -2,16 +2,62 @@
  * @param {number[][]} grid
  * @return {number}
  */
+
+function helper (array, xIndex, yIndex) {
+  console.log(xIndex,yIndex);
+  let helperObject = {
+    up:null,
+    right:null,
+    down:null,
+    left:null,
+  };
+
+  //check Up
+  if(yIndex-1 >= 0){
+    helperObject.up = array[yIndex-1][xIndex];
+  } else {
+    helperObject.up = 0;
+  }
+
+  //check Right
+  if(xIndex+1 <= array[yIndex].length-1){
+    helperObject.right = array[yIndex][xIndex+1];
+  } else {
+    helperObject.right = 0;
+  }
+
+  //check Down
+  if(yIndex+1 <= array.length-1){
+    helperObject.down = array[yIndex+1][xIndex];
+  } else {
+    helperObject.down = 0;
+  }
+
+  //check Left
+  if(xIndex-1 >= 0){
+    helperObject.left = array[yIndex][xIndex-1];
+  } else {
+    helperObject.left = 0;
+  }
+
+  return helperObject;
+}
+
 var minDays = function (grid) {
+  for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[i].length; j++){
+      console.log(helper(grid,j,i));
+    }
+  }
   // is there any index that only has 1 neighbour
   // else 
 };
 
 const grid = [
-  [1, 1, 0, 1, 1],
-  [1, 1, 1, 1, 1],
-  [1, 1, 0, 1, 1],
-  [1, 1, 0, 1, 1]
+  [1, 1, 0, 1, 1,1],
+  [1, 1, 1, 1, 1,0],
+  [1, 1, 0, 1, 1,1],
+  [1, 1, 1, 1, 1,0]
 ];
 
 const grid2 = [
