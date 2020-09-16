d82ba599a8e32bbdcec59c5c9ed9220d47576c78
nickthorpe71 <nickthorpe71@gmail.com>
2020-09-16 08:13:57 -0500

    adds reverse linked list

--- 9-17-2020-reverse-linked-list.js
+++ 9-17-2020-reverse-linked-list.js
@@ -7,15 +7,57 @@ for (const i of data) {
   list.push(i);
 }
 
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var reverseList = function (head) {
+  let node = head;
+  let prev;
+  let temp;
+
+  while (node) {
+    temp = node.next;
+
+    node.next = prev;
+
+    prev = node;
+    node = temp;
+  }
+
+  return prev;
+};
+
+
+console.log(reverseList(list.head));
+
+
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseListWithExtraList = function (head) {
   let result = new LinkedList();
 
+  for (let i = 0; i < 5; i++) {
+    let current = head;
+    let prev = head;
 
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
 
+    result.push(current.element);
+    prev.next = null;
+  }
+
+  return result.head;
 };
+
+
 
 
