3934417ad3298924f2331c0ed220e7113e534c62
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-18 07:37:57 -0600

    adds graph creation and population for squareSum

--- 1-18-2020-SquareSumsAttempt2.js
+++ 1-18-2020-SquareSumsAttempt2.js
@@ -1,11 +1,38 @@
 
 
 function square_sums_row(n) {
-    let squares = getSquares(n);
-    return squares[squares.length - 1];
+    const squares = getSquares(n);
+    const graphMap = createGraph(n);
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (squares.includes(i + j) && j != i) {
+                graphMap.get(i).edges.push(graphMap.get(j));
+            }
+        }
+    }
+
+    return graphMap;
 }
 
-// Gets all squared numbers that a number + itself or less could equal
+/**
+ * @param {Number} n 
+ * @returns {hashMap} map containing keys 1 - n. Each has a starting node with no edges
+ */
+function createGraph(n) {
+    let map = new Map();
+
+    for (let i = 1; i <= n; i++) {
+        map.set(i, new Node(i));
+    }
+
+    return map;
+}
+
+/**
+ * @param {Number} n 
+ * @returns {Array} array of all squares that fall within n + n
+ */
 function getSquares(n) {
     const arr = [];
     let i = 0;
@@ -16,6 +43,17 @@ function getSquares(n) {
     } while (i * i < n + n);
 
     return arr;
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.edges = [];
+    }
+
+    addEdge(node) {
+        edges.push(node);
+    }
 }
 
 console.log(square_sums_row(15));
